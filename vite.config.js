bd3b78b23f6dd22b1b68c8266ef60c071ff311fd
taryarvik <taryarvik@gmail.com>
2026-02-25 04:54:46 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
-  theme: { extend: {} },
-  plugins: [],
-};
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()]
+})
